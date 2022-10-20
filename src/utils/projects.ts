01a93020c3cc3ac9a0bf327f88372a4a0868118a
Julio Zambom <work.juliozambom@gmail.com>
2022-10-20 19:12:53 -0300

    feat: creating projects utils

---
+++ src/utils/projects.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: 'BetterWorld',
+    area: 'Front-end',
+    description:
+      'Uma simples landing page de uma empresa fictícia que visa ajudar o meio ambiente.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    github: '',
+    deploy: '',
+    image: 'https://i.imgur.com/8Z7Z7Zu.png',
+  },
+];
